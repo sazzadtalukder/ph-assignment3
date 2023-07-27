557da69617ccf9cdb9e73e46c6a7162d52fde5af
Md. Sazzad Talukder <sazzadtalukdercseiu@gmail.com>
2023-07-27 11:06:17 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+
+  module.exports = {
+    theme: {
+      colors: {
+        
+        'primaryB': '#F85559',
+        
+      },
+    },
+  }
